6c6464fed5f4f029230e6274acdb046f7ec0f48d
multiserviciosdorita3-ux <multiserviciosdorita3@gmail.com>
2026-05-05 11:24:44 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
-var destinos = [
+export const destinos = [
     {
-        titulo: "Europa",
+        titulo: "Europa Premium",
         descripcion: "Viajes culturales",
         imagen: "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
     },
     {
-        titulo: "Caribe",
+        titulo: "Caribe Todo Incluido",
         descripcion: "Playas paradisíacas",
         imagen: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
     }
 ];
 
-var servicios = [
+export const servicios = [
     {
         titulo: "Paquetes turísticos",
         descripcion: "Todo incluido"
@@ -22,7 +22,7 @@ var servicios = [
     }
 ];
 
-var paquetes = [
+export const paquetes = [
     {
         titulo: "Europa Premium",
         precio: "$1200",
@@ -34,5 +34,17 @@ var paquetes = [
         horarios: "8:00 AM - 8:00 PM",
         plan: "Hotel + transporte + guía",
         opiniones: "★★★★★ Excelente servicio"
+    },
+    {
+        titulo: "Caribe Todo Incluido",
+        precio: "$900",
+        descripcion: "Resort + playa + comida",
+        imagen: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+        galeria: [
+            "https://images.unsplash.com/photo-1506929562872-bb421503ef21"
+        ],
+        horarios: "10:00 AM",
+        plan: "Todo incluido",
+        opiniones: "★★★★★ Increíble experiencia"
     }
 ];
